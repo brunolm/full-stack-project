0126c34dd729da9a2c73d8070fa20a777b1f0ac8
Bruno Leonardo Michels <brunolm7@gmail.com>
2020-01-28 18:38:03 -0300

    Fix lint

--- api/src/services/user/queries.ts
+++ api/src/services/user/queries.ts
@@ -2,7 +2,7 @@ import { db } from '../core/db'
 import { tables, userColumns } from '../core/db/schema'
 
 export const userQueries = {
-  User: async (parent, { id }, context) => {
+  User: async (_parent, { id }, _context) => {
     return await db(tables.users)
       .where(userColumns.id, id)
       .first()
